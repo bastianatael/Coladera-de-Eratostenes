3192f0890a69a5c2322ef203503a6a4d95f10dae
Bastian Natael Hernández Corona <bhernandez20@ucol.mx>
2020-10-12 22:11:28 -0500

    First commit

--- practica.js
+++ practica.js
@@ -1,28 +1,43 @@
-class ColaderaDeEratostenes{
-    criba(){
-    var m = [];
-    var mPrimos = [];
-
-    for(let i = 0; i < 1000; i++ ){
-        m[i] = 1;
-    }
-
-    for(var i = 2; i < m.length; i++){
-            for (var j = i * i; j < m.length; j += i) {
-                m[j] = 0;
-            }
-    }
-
-    let indice = 0;
-    for(let i = 0; i < m.length; i++){
-        if(m.findIndex(indice => indice == 1) == i){
-            mPrimos[indice++] = i;
-            m[i] = 0;
-        }
-    }
-
-    console.log(mPrimos)
+class Dado{
+    lanzar(){
+        return Math.ceil(Math.random()*6);
     }
 }
-let app=new ColaderaDeEratostenes();
-app.criba();
+class Corredor{
+    constructor(nombre){
+        this.nombre=nombre;
+        this.dado=new this.dado();
+        this.posicion=0;
+    }
+    correr(){
+        let cara=this.dado.lanzar();
+        if(rand==1){
+            this.posicion+=3;
+        }
+        else if(cara<=3){
+            this.posicion++;
+        }
+        else if(cara<=6){
+            this.posicion+=2;
+        }
+    }
+}
+class Carrera{
+    constructor(){
+        this.c1=new Corredor("Bastian");
+        this.c2=new Corredor("Goku");
+        this.distancia=100;
+    }
+
+    iniciar(){
+        while(this.c1.posicion<this.distancia && this.c2.posicion<this.distancia){
+        this.c1.correr();
+        this.c2.correr();
+        console.log(this.c1.posicion+": : :"+this.c2.posicion);
+        }
+        if(c1>=this.distancia && c2>=this.distancia) console.log("Hubo empate")
+        else if(c1>=this.distancia){
+            console.log("c1 ganó")
+        } else console.log("c2 ganó")
+    }
+}
